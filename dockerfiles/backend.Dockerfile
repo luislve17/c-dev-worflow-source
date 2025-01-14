17eaf1d35ed5ed58ac06419b5227f2c3e98745c7
Luis Vasquez <luislve1796@gmail.com>
2025-01-13 21:56:45 -0500

    Problem statement and simple implementation

--- dockerfiles/backend.Dockerfile
+++ dockerfiles/backend.Dockerfile
@@ -3,5 +3,6 @@ FROM python:3.13-bookworm
 WORKDIR /app
 
 COPY ./src /app
+RUN pip install -r requirements.txt
 
-CMD ["python", "main.py"]
+CMD ["uvicorn", "main:app", "--host", "0.0.0.0", "--port", "8000", "--reload"]
--- src/main.py
+++ src/main.py
@@ -1,1 +1,36 @@
-print("Hello world!!!")
+from time import sleep
+from math import sqrt
+from fastapi import FastAPI, HTTPException
+import json
+
+app = FastAPI()
+
+
+@app.post("/sum-and-square")
+def read_item(body: dict):
+    # Get variables
+    a = body.get("a")
+    b = body.get("b")
+
+    # Process
+    try:
+        result = super_complex_calculation(a, b)
+    except ValueError as e:
+        raise HTTPException(
+            status_code=422, 
+            detail=f"Error handling 'a' and 'b': {str(e)}"
+        )
+
+    # Return result
+    return {"result": result}
+
+
+def super_complex_calculation(a, b):
+    try:
+        num_a = float(a)
+        num_b = float(b)
+    except (TypeError, ValueError):
+        raise ValueError("Invalid inputs for calculation")
+
+    sleep(3)  # Turning on nuclear reactor
+    return sqrt(num_a + num_b)
